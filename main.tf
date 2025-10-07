f7634e16c839c78ccaf25595c2260e732088bd37
tomneil1997 <thomas.neil@onr.gov.uk>
2025-10-07 16:29:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG3"
+    resource_group_name  = "RG1"
     storage_account_name = "jenkinsstatetom"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
@@ -22,8 +22,9 @@ terraform {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "RG2"
+  name     = "RG3"
   location = "westeurope"
 }
+
 
 
